3f438488c002f94e38d3d454baf2320f82472d03
MichelleRP <michelle.ruiz07@hotmail.com>
2016-09-26 19:42:44 -0600

    unidad 1
    angularj s

---
+++ unidad1/BookStoreApp/www/src/controllers/LibrosListController.js
@@ -0,0 +1,8 @@
+function LibrosListController($scope, $http) {
+  $http.get('data/libros.json').success(function(data) {
+	$scope.libros = data;
+  });
+ 
+  $scope.orderField = "titulo";
+  $scope.orderReverse = "true";
+}
